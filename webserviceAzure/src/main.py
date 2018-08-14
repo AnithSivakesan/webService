74a3ae85a57cacc0aac7a04d16a2bc2468381936
SIVA5239 <SIVA5239@LIN330111.wsatkins.com>
2018-08-14 19:44:10 +0530

    commit1

---
+++ webserviceAzure/src/main.py
@@ -0,0 +1,16 @@
+'''
+Created on 11 Jul 2018
+
+@author: SIVA5239
+'''
+
+from flask.app import Flask
+app=Flask(__name__)
+
+@app.route('/')
+def helloWorld():
+    return "hello world"
+
+if __name__=='__main__':
+    app.run()
+    
